7657351d331b0a032770c7b0f9bc673c1aa386ca
Shea Wojciehowski <shea.e.wojciehowski.17@dartmouth.edu>
2018-01-10 23:20:14 -0500

    added secret keys, access tokens and initial tutorial files

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumer_key: 'oDIyE5viT9CgYXagoVagyRgNr',
+    consumer_secret: 'xMGClKWafhmOvcwGwxbhWNFhzQEdlbrSrkAtdrEYYn9LPObloG',
+    access_token_key: '951296471692955649-qT9o4rh1l5LBZnBi8X55G0Fxdq7nApO',
+    access_token_secret: 'Goq0tzvae0AEX2qUWJbkK0QSILTIR1y2p9hLZkQPrMQoE'
+  }
+}
